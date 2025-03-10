5967e1fb9886feb64fffefd2839a16063b4aadee
SamuelArenas2005 <e.sir.samuel.arenas@cali.edu.co>
2025-03-10 01:47:50 -0500

    first commit

---
+++ src/main/scala/Derivacion/package.scala
@@ -0,0 +1,29 @@
+package object Derivacion {
+  def derivada(f:Double => Double): Double => Double = {
+    def prueba(x: Double):Double = {x}
+    prueba
+  }
+
+  def derivadaSuma(f: Double => Double, g: Double => Double): Double => Double = {
+    def prueba(x: Double): Double = {x}
+    prueba
+  }
+
+  def derivadaResta(f: Double => Double, g: Double => Double): Double => Double = {
+    def prueba(x: Double): Double = {x}
+    prueba
+  }
+
+  def derivadaMult(f: Double => Double, g: Double => Double): Double => Double = {
+    def prueba(x: Double): Double = {x}
+    prueba
+  }
+
+  def derivadaDiv(f: Double => Double, g: Double => Double): Double => Double = {
+    def prueba(x: Double): Double = {x}
+    prueba
+  }
+
+
+
+}
