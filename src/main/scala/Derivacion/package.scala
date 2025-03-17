54746cf39b6cf2882ba44c3d56029c9ae8c5b421
SamuelArenas2005 <e.sir.samuel.arenas@cali.edu.co>
2025-03-17 17:05:13 -0500

    cambios

--- src/main/scala/Derivacion/package.scala
+++ src/main/scala/Derivacion/package.scala
@@ -1,27 +1,41 @@
 package object Derivacion {
+  val h = 0.1;
   def derivada(f:Double => Double): Double => Double = {
-    def prueba(x: Double):Double = {x}
-    prueba
+    def derivadaFunction(x: Double):Double = {
+      (f(x-2*h)-8*f(x-h)+8*f(x+h)-f(x+2*h))/(12*h)
+    }
+    derivadaFunction
   }
 
   def derivadaSuma(f: Double => Double, g: Double => Double): Double => Double = {
-    def prueba(x: Double): Double = {x}
-    prueba
+    def derivadaSumaFunction(x: Double): Double = {
+      derivada(f)(x) + derivada(g)(x)
+    }
+
+    derivadaSumaFunction
   }
 
   def derivadaResta(f: Double => Double, g: Double => Double): Double => Double = {
-    def prueba(x: Double): Double = {x}
-    prueba
+    def derivadaRestaFunction(x: Double): Double = {
+      derivada(f)(x) - derivada(g)(x)
+    }
+    derivadaRestaFunction
   }
 
   def derivadaMult(f: Double => Double, g: Double => Double): Double => Double = {
-    def prueba(x: Double): Double = {x}
-    prueba
+    def derivadaMultFunction(x: Double): Double = {
+      derivada(f)(x)*g(x) + f(x)*derivada(g)(x)
+    }
+
+    derivadaMultFunction
   }
 
   def derivadaDiv(f: Double => Double, g: Double => Double): Double => Double = {
-    def prueba(x: Double): Double = {x}
-    prueba
+    def derivadaDivFunction(x: Double): Double = {
+      (derivada(f)(x) * g(x) - f(x) * derivada(g)(x))/(g(x)*g(x))
+    }
+
+    derivadaDivFunction
   }
 
 
--- src/test/scala/pruebas.sc
+++ src/test/scala/pruebas.sc
@@ -1,2 +1,8 @@
 import Derivacion. _
+def cuadratica(x:Double):Double = x*x
+def cubica(x:Double):Double = x*x*x
+
+def derivacion = derivadaMult(cuadratica,cubica)
+
+derivacion(2)
 
